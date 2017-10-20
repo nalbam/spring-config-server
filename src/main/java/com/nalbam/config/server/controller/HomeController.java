f3cca4b994f47e2f777bde75a49fb1c3ff29aa52
nalbam-ya <jyyu@yanolja.com>
2017-10-20 18:15:21 +0900

    lombok

--- src/main/java/com/nalbam/config/server/controller/HomeController.java
+++ src/main/java/com/nalbam/config/server/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.nalbam.config.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
-public class HealthController {
+public class HomeController {
 
-    @GetMapping
+    @GetMapping("/health")
     public String health() {
         return "OK";
     }
--- src/test/java/com/nalbam/config/server/ConfigServerApplicationTests.java
+++ src/test/java/com/nalbam/config/server/ConfigServerApplicationTests.java
@@ -1,42 +1,21 @@
 package com.nalbam.config.server;
 
-import org.junit.Assert;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ConfigServerApplication.class, webEnvironment = RANDOM_PORT)
 public class ConfigServerApplicationTests {
 
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
-
-    @LocalServerPort
-    private final int port = 0;
-
     @Test
-    public void health() {
-        this.log.info("health port : " + this.port);
-
-        final String url = "http://localhost:" + this.port + "/health";
-
-        final RestTemplate restTemplate = new RestTemplate();
-
-        final ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-        this.log.info("health code : " + response.getStatusCode());
-        this.log.info("health body : " + response.getBody());
-
-        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+    public void contextLoads() {
+        log.debug("## contextLoads.");
     }
 
 }
