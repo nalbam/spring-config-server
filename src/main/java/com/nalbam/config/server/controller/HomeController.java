1f20df62cfb980ed8d117d53e77053051bb7d9c5
nalbam <me@nalbam.com>
2018-04-23 14:57:39 +0900

    eureka-client

--- src/main/java/com/nalbam/config/server/controller/HomeController.java
+++ src/main/java/com/nalbam/config/server/controller/HomeController.java
@@ -1,16 +1,31 @@
 package com.nalbam.config.server.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 public class HomeController {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/health")
     public String health() {
         return "OK";
+    }
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
 
 }
